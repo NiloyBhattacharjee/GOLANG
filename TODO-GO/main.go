0596aa2ce06fa78df17254f0d69b00f81f2fe892
NiloyBhattacharjee <nlbhattacharjee20@gmail.com>
2022-03-27 03:44:51 -0400

    go-server

--- TODO-GO/main.go
+++ TODO-GO/main.go
@@ -1,36 +1,31 @@
 package main
 
-import{
+import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
-}
-
-
-
+)
 
 type todo struct {
-	ID			string 	`json:"id"`
-	Item		string	`json:"title"`
-	Completed	bool 	`json:"completed"`	
+	ID        string `json:"id"`
+	Item      string `json:"title"`
+	Completed bool   `json:"completed"`
 }
 
-
-var todos = []{
-{ID: "1" , Item: "clean Book" , Completed:false},
-{ID: "2" , Item: "Study" , Completed:false},
-{ID: "3" , Item: "Die" , Completed:false},
-{ID: "4" , Item: "MAMA" , Completed:false},	
-{ID: "5" , Item: "Just killed a man" , Completed:false},
+var todos = []todo{
+	{ID: "1", Item: "clean Book", Completed: false},
+	{ID: "2", Item: "Study", Completed: false},
+	{ID: "3", Item: "Die", Completed: false},
+	{ID: "4", Item: "MAMA", Completed: false},
+	{ID: "5", Item: "Just killed a man", Completed: false},
 }
 
-func getTodos(context){
-	
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
 }
 
-
-
-func main()  {
+func main() {
 	router := gin.Default()
-	router.GET("/todos")
-	router.Run("localhost:9090")	
+	router.GET("/todos", getTodos)
+	router.Run("localhost:9090")
 }
